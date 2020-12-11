b730631a5eebd3d43c3b150110e4c1f8baadacd4
mjperezg2010 <mjperezg2010@unitec.edu>
2020-12-11 15:41:42 -0600

    cambios en SVM

--- SVM.py
+++ SVM.py
@@ -1,10 +1,9 @@
 
 import utilities as util
 from sklearn.svm import SVC as model_svm
+import numpy as np
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    main()
+
 
 def main():
     
@@ -29,18 +28,28 @@ def main():
                         kernel = kernel,
                         gamma = g
                     )
-                    groups_X,groups_Y = util.split_data(groups,X,Y)
+                    groups_X, groups_Y = util.split_data(groups, X, Y)
+                    groups_Y = groups_Y.astype('int')
                     for i in range(groups):
                         validation_X = groups_X[i]
                         validation_Y = groups_Y[i]
-                        for j in range(groups):
+                        if i != 0:
+                            training_X = groups_X[0]
+                            training_Y = groups_Y[0]
+                        else:
+                            training_X = groups_X[1]
+                            training_Y = groups_Y[1]
+                        for j in range(1, groups):
                             if j != i:
-                                training_X += groups_X[i]
-                                training_Y += groups_Y[i]                            
-                        model.fit(training_X,training_Y)
-                        F1_val = util.F1(model,validation_X,validation_Y)
-                        print(a+" "+c+" "+kernel+" "+g)
+                                training_X = np.concatenate((training_X, groups_X[i]))
+                                training_Y = np.concatenate((training_Y, groups_Y[i]))
+
+                        model.fit(training_X, training_Y)
+                        F1_val = util.F1(model, validation_X, validation_Y)
+                        print(a,c,kernel,g,F1_val)
                             
 
-    
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
 
