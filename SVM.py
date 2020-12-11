2016e544eea75c99befeb856f40bfe2a5d89385a
bManrique17 <bmanrique17@gmail.com>
2020-12-11 15:24:20 -0600

    asdasd

--- SVM.py
+++ SVM.py
@@ -1,6 +1,6 @@
 
 import utilities as util
-from sklearn.svm import SVC as model
+from sklearn.svm import SVC as model_svm
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -12,35 +12,34 @@ def main():
                     "laboratorio_train_synth_dengue.csv",
                     "completo_train_synth_dengue.csv"]
     
-    trees = [ i*10 for i in range(5) ]    
-    criterion = ['gini','entropy']
-    max_depth = [None,3,6]
-    max_features = ['auto', 'log2']
+    C = [ i/10 for i in range(1,15) ]
+    kernels = ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed']
+    gamma = ['scale', 'auto']+[ i/10 for i in range(1,15) ]
+    
     groups = 5
 
     for a in data_sets:
         X,Y = util.process_data(a)
-        for b in trees:
-            for c in criterion:
-                for d in max_depth:
-                    for e in max_features:                 
-                        model = model(
-                            n_estimators = b,
-                            criterion = c,
-                            max_depth = d,
-                            max_features = e
-                        )
-                        groups_X,groups_Y = util.split_data(groups,X,Y)
-                        for i in range(groups):
-                            validation_X = groups_X[i]
-                            validation_Y = groups_Y[i]
-                            for j in range(groups):
-                                if j != i:
-                                    training_X += groups_X[i]
-                                    training_Y += groups_Y[i]                            
-                            model.fit(training_X,training_Y)
-                            F1_val = util.F1(model,validation_X,validation_Y)
-                            print(util.print_data(a,b,c,d,e,i,F1_val))
+        X = util.normalizacion(X)
+        for c in C:
+            for kernel in kernels:
+                for g in gamma:                    
+                    model = model_svm(
+                        C = c,
+                        kernel = kernel,
+                        gamma = g
+                    )
+                    groups_X,groups_Y = util.split_data(groups,X,Y)
+                    for i in range(groups):
+                        validation_X = groups_X[i]
+                        validation_Y = groups_Y[i]
+                        for j in range(groups):
+                            if j != i:
+                                training_X += groups_X[i]
+                                training_Y += groups_Y[i]                            
+                        model.fit(training_X,training_Y)
+                        F1_val = util.F1(model,validation_X,validation_Y)
+                        print(a+" "+c+" "+kernel+" "+g)
                             
 
     
